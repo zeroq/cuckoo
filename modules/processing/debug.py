f63d0783b2787ebb8058a71f1ada6a3bc1775deb
zeroq <root@cert.siemens.com>
2013-05-08 12:33:03 +0200

    added retries if database is locked

--- modules/processing/debug.py
+++ modules/processing/debug.py
@@ -4,6 +4,7 @@
 
 import os
 import codecs
+import time
 
 from lib.cuckoo.common.abstracts import Processing
 from lib.cuckoo.common.exceptions import CuckooProcessingError
@@ -27,7 +28,18 @@ class Debug(Processing):
             except (IOError, OSError) as e:
                 raise CuckooProcessingError("Error opening %s: %s" % (self.log_path, e))
 
-        for error in Database().view_errors(int(self.cfg.analysis.id)):
-            debug["errors"].append(error.message)
+        ### JG: added retries to avoid error on database locked
+        retries = 5
+        worked = False
+
+        while not worked and retries > 0:
+            try:
+                for error in Database().view_errors(int(self.cfg.analysis.id)):
+                    debug["errors"].append(error.message)
+                worked = True
+                retries = 0
+            except:
+                retries -= 1
+                time.sleep(5)
 
         return debug

121b3bd694b0355a821d66fed1a9b717027ecb52
root <root@charon.cert.siemens.com>
2015-01-16 11:00:33 +0100

    utility to get the machine for a given analysis id

---
+++ utils/get_machine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Copyright (C) 2010-2014 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+import os.path
+import sys
+import time
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+from lib.cuckoo.core.database import Database, TASK_PENDING, TASK_RUNNING
+from lib.cuckoo.core.database import TASK_COMPLETED, TASK_RECOVERED
+from lib.cuckoo.core.database import TASK_REPORTED, TASK_FAILED_ANALYSIS
+from lib.cuckoo.core.database import TASK_FAILED_PROCESSING
+
+def timestamp(dt):
+    """Returns the timestamp of a datetime object."""
+    return time.mktime(dt.timetuple())
+
+def main(analysis_id):
+    db = Database()
+    analysis_task = db.view_task(analysis_id)
+    if analysis_task:
+        print analysis_task.machine
+        return
+    print "NotSet"
+    return
+
+if __name__ == "__main__":
+    analysis_id = sys.argv[1]
+    main(analysis_id)
